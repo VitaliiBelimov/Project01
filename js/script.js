e756439403c112f30affefb0834ade6b68fd4797
Vitalii Bielimov <vitalii.belimov@gmail.com>
2020-10-18 18:40:36 +0300

    Lesson 012

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-alert('Hello world!');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a1 = prompt('Один из просмотренных фильмов:', ''),
+      b1 = prompt('На сколько оцените его?', ''),
+      a2 = prompt('Один из просмотренных фильмов:', ''),
+      b2 = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a1] = b1;
+personalMovieDB.movies[a2] = b2;
+
+console.log(personalMovieDB);
